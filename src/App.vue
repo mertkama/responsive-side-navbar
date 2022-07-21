98c68f160831404b905c800315217b526d9d7c51
INVEKOR\Administrator <mert1723@gmail.com>
2022-07-21 17:37:13 +0300

    color changed.

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,12 @@ import Sidebar from './components/Sidebar.vue'
 
 <style lang="scss">
 :root {
-	--primary: #4ade80;
+	--primary: #d24ade;
 	--primary-alt: #22c55e;
 	--grey: #64748b;
-	--dark: #1e293b;
-	--dark-alt: #334155;
-	--light: #f1f5f9;
+	--dark: #5543b9;
+	--dark-alt: #1b2f4b;
+	--light: #d0d1d1;
 	--sidebar-width: 300px;
 }
 
